2c38186124d704e07c09b8b70e2e4811276d66b5
gifnksm <makoto.nksm+github@gmail.com>
2019-11-10 21:11:46 +0900

    chapter 7: colorful numbers

--- src/syntax.rs
+++ src/syntax.rs
@@ -20,18 +20,28 @@ impl Highlight {
 
 impl Row {
     pub(crate) fn update_syntax(&mut self) {
-        self.hl = self
-            .render
-            .chars()
-            .flat_map(|ch| {
-                let byte = ch.len_utf8();
-                let hl = if ch.is_digit(10) {
-                    Highlight::Number
-                } else {
-                    Highlight::Normal
-                };
-                iter::repeat(hl).take(byte)
-            })
-            .collect();
+        self.hl.clear();
+
+        let mut prev_sep = true;
+        let mut prev_hl = Highlight::Normal;
+
+        for ch in self.render.chars() {
+            let (hl, is_sep) = if ch.is_digit(10) && (prev_sep || prev_hl == Highlight::Number)
+                || (ch == '.' && prev_hl == Highlight::Number)
+            {
+                (Highlight::Number, false)
+            } else {
+                (Highlight::Normal, is_separator(ch))
+            };
+
+            self.hl.extend(iter::repeat(hl).take(ch.len_utf8()));
+
+            prev_hl = hl;
+            prev_sep = is_sep;
+        }
     }
+}
+
+fn is_separator(ch: char) -> bool {
+    ch.is_whitespace() || ch == '\0' || ",.()+-/*=~%<>[];".contains(ch)
 }
